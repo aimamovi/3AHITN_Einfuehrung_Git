f97b8c5bf5c1ad9f6ba8c186ddeba5892cd3c5c9
ArminCodingInDaSchool <apoljo@htl-steyr.ac.at>
2020-11-11 11:56:42 +0100

    yea boi armin did it again

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Selam alejkum Aldine sta ima?");
         System.out.println("Hallo du peder!");
         System.out.println("kuuuuuuuuuuuuuuuuuuuuuuuuuuuuurac braaaaaaaaaa");
+        System.out.println("ein test um 11:55");
 
     }
 
